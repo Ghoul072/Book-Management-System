5e539fa3f4821bd051f71f3d4a61b5f779331480
Yaseen <ahm3d.yaseen@gmail.com>
2023-11-19 22:45:46 +0500

    add auth api views

--- book_management_system/api/urls.py
+++ book_management_system/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from . import views
 
 urlpatterns = [
     path('books/', views.BookCView.as_view(), name='book-c'),               # POST: Add a new book
     path('books/<int:pk>/', views.BookRUDView.as_view(), name='book-rud'),  # GET, PUT, DELETE by ID
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
