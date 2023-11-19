80afa28aed0c5f856c5b4a8bec580652297c0fff
Yaseen <ahm3d.yaseen@gmail.com>
2023-11-19 22:00:16 +0500

    add pagination

--- book_management_system/api/views.py
+++ book_management_system/api/views.py
@@ -3,13 +3,19 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+
+class BooksPagination(PageNumberPagination):
+    page_size = 10  # Number of items per page
+    page_size_query_param = 'page_size'
+    max_page_size = 1000 
 
 # Book Create view (the C in CRUD)
 class BookCView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    pagination_class = BooksPagination
 
 # Book Read, Update, Delete view (RUD in CRUD)
 class BookRUDView(generics.RetrieveUpdateDestroyAPIView):
