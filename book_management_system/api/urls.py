a75f192636bf254535afbc1e75b1e0a32cc7a942
Yaseen <ahm3d.yaseen@gmail.com>
2023-11-19 23:50:40 +0500

    path correction

--- book_management_system/api/urls.py
+++ book_management_system/api/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('books/', views.BookCView.as_view(), name='book-c'),               # POST: Add a new book
     path('books/<int:pk>/', views.BookRUDView.as_view(), name='book-rud'),  # GET, PUT, DELETE by ID
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
