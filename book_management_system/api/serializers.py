38eda4ce8260471875c4f63b514693dd54c84b86
Yaseen <ahm3d.yaseen@gmail.com>
2023-11-19 20:22:15 +0500

    add publicationYear validation

--- book_management_system/api/serializers.py
+++ book_management_system/api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
+from django.core.validators import MinValueValidator
+from datetime import datetime as dt
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ('id', 'title', 'author', 'publicationYear', 'genre')
+        
+    def validate_publicationYear(self, value):
+        if value > dt.now().year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
