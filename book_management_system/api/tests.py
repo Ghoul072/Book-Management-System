c849038c6192de5bba33eb07bd10a29292cacca4
Yaseen <ahm3d.yaseen@gmail.com>
2023-11-19 20:23:18 +0500

    remove redundant import

--- book_management_system/api/tests.py
+++ book_management_system/api/tests.py
@@ -4,7 +4,6 @@ from rest_framework import status
 from datetime import datetime as dt
 
 from .models import Book
-from .serializers import BookSerializer
 
 class BookAPITests(TestCase):
     def setUp(self) -> None:
