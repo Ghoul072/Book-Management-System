85ae8ee11a4d59c25be3976f2fe1a0caffd56f86
Yaseen <ahm3d.yaseen@gmail.com>
2023-11-19 22:15:38 +0500

    rearrange imports

--- book_management_system/api/views.py
+++ book_management_system/api/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.authentication import TokenAuthentication
+
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.pagination import PageNumberPagination
 
 
 class BooksPagination(PageNumberPagination):
