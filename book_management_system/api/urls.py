61c10c26b71112c005180d9210bcd41509fa0704
Yaseen <ahm3d.yaseen@gmail.com>
2023-11-19 19:53:35 +0500

    add urls

--- book_management_system/api/urls.py
+++ book_management_system/api/urls.py
@@ -2,4 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('books/', views.BookCView.as_view(), name='book-c'),               # POST: Add a new book
+    path('books/<int:pk>/', views.BookRUDView.as_view(), name='book-rud'),  # GET, PUT, DELETE by ID
 ]
