4e017345198463a069ae4287464712cf43b2dd5b
Yaseen <ahm3d.yaseen@gmail.com>
2023-11-19 19:53:02 +0500

    create required views for CRUD operations

--- book_management_system/api/views.py
+++ book_management_system/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
 # Create your views here.
+
+# Book Create view (the C in CRUD)
+class BookCView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# Book Read, Update, Delete view (RUD in CRUD)
+class BookRUDView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
