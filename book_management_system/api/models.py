ee5e8f87c638c6224f78bfc8ccd4f8971bf1e065
Yaseen <ahm3d.yaseen@gmail.com>
2023-11-19 21:03:24 +0500

    switch to validating directly in django model

--- book_management_system/api/models.py
+++ book_management_system/api/models.py
@@ -1,12 +1,18 @@
 from django.db import models
-
-# Create your models here.
+from django.core.validators import MinValueValidator, MaxValueValidator
+from datetime import datetime as dt
     
 class Book(models.Model):
-    id = models.AutoField(primary_key=True)  # Auto-incrementing integer ID
-    title = models.CharField(max_length=255)  # String field for the title
-    author = models.CharField(max_length=255)  # String field for the author's name
-    publicationYear = models.IntegerField()  # Integer field for publication year
+    id = models.AutoField(primary_key=True)     # Auto-incrementing integer ID
+    title = models.CharField(max_length=255)    # String field for the title
+    author = models.CharField(max_length=255)   # String field for the author's name
+    publicationYear = models.IntegerField(      # Integer field for publication year
+        validators=[
+            MinValueValidator(1, message="Publication year cannot be less than 1"), # Assuming books were not published in BC time periods. This can be edited to accomodate BC years
+            MaxValueValidator(dt.today().year, message="Publication year cannot be in the future")
+        ],
+        null=True, blank=True # In the case where publication year is unknown, allow to leave this field blank
+        ) 
     genre = models.CharField(max_length=100)  # String field for the genre
     
     def __str__(self):
--- book_management_system/api/serializers.py
+++ book_management_system/api/serializers.py
@@ -1,20 +1,8 @@
 from rest_framework import serializers
-from django.core.validators import MinValueValidator
-from datetime import datetime as dt
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
     
-    # Validation for required fields
-    title = serializers.CharField(required=True)
-    author = serializers.CharField(required=True)
-    
-    
     class Meta:
         model = Book
         fields = ('id', 'title', 'author', 'publicationYear', 'genre')
-        
-    def validate_publicationYear(self, value):
-        if value > dt.now().year:
-            raise serializers.ValidationError("Publication year cannot be in the future.")
-        return value
